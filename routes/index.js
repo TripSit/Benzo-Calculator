5f3859151259dcda88811ecbbc583a3d16b6a5d2
Spacebjorn <sebastianwith@gmail.com>
2018-02-25 20:13:30 +0000

    Everything is work, maybe, and is formatted neatly

--- routes/index.js
+++ routes/index.js
@@ -5,50 +5,65 @@ var router = express.Router();
 
 var drugCache = {};
 var benzoCache = {};
-var updateCache = function() {
-  try {
-    // Get the drugs
-    request.get('http://tripbot.tripsit.me/api/tripsit/getAllDrugs', {
-      'json': true,
-    }, function(request, response, body) {
-      try { // Get cache
-        drugCache = body.data[0];
-        benzoCache = {};
-        // Filter any drug not containing the dose_to_diazepam property
-        benzoCache = _.filter((drugCache), function(drugCache) { return _.has(drugCache.properties, 'dose_to_diazepam'); });
-        // Get all aliases and assign the same values as the real name
-        _.each(benzoCache, function(d) {
-          _.each(d.aliases, function(a) {
-            benzoCache.push({ 
-                name: a,
-                pretty_name: a,
-                properties: d.properties,
-              }); 
-          }); 
-        });
-        benzoCache = _.sortBy(benzoCache, 'name');
-        // Apply a value Select2 can use to calculate with
-        var regex = /[0-9]+\.?[0-9]?/; // search for the first number sequence of a number in a sentence
-        benzoCache = _.each(benzoCache, function(benzoCache){
-           benzoCache.diazvalue = regex.exec(benzoCache.properties.dose_to_diazepam);
-        });
-        
-      } catch (err) {}
-    });
-  } catch (err) {}
-};
 
+function getAlias() {
+ _.each(benzoCache, function(benzo) {
+  _.each(benzo.aliases, function(alias) {
+   benzoCache.push({
+    // Add used properties to new objects
+    name: alias,
+    pretty_name: alias.charAt(0).toUpperCase() + alias.slice(1),
+    properties: benzo.properties,
+    formatted_dose: benzo.formatted_dose,
+    });
+  });
+ }); 
+}
+
+function sortCache() {
+  benzoCache = _.sortBy(benzoCache, 'name');
+  // Apply a value Select2 can use to calculate with
+  var regex = /[0-9]+\.?[0-9]?/; // search for the first number sequence of a number in a sentence
+  benzoCache = _.each(benzoCache, function(benzoCache) {
+    benzoCache.diazvalue = regex.exec(benzoCache.properties.dose_to_diazepam);
+  });
+}
+
+function formatCache() {
+  benzoCache = {};
+        // Filter any drug not containing the dose_to_diazepam property
+        benzoCache = _.filter((drugCache), function(drugCache) {
+          return _.has(drugCache.properties, 'dose_to_diazepam');
+        });
+}
+
+var updateCache = function() {
+    // Get the drugs
+ request.get('http://tripbot.tripsit.me/api/tripsit/getAllDrugs', {
+  'json': true,
+ }, 
+ function(request, response, body) {
+    // Get cache
+    drugCache = body.data[0];
+    // Format Dose
+    formatCache();
+    // Get all aliases
+    getAlias();
+    // Sort cache
+    sortCache();
+  });
+};
 
 // Update from our cache every minute, update from erowid's api every 60 minutes
 setInterval(updateCache, 60000);
 updateCache();
 
 // Routes
-router.get("/", function(req, res){
-//   var drugs = _.sortBy(drugCache, 'name');
-   res.render("index",{benzo: benzoCache});
+router.get("/", function(req, res) {
+  //   var drugs = _.sortBy(drugCache, 'name');
+  res.render("index", {
+    benzo: benzoCache
+  });
 });
-
-
 
 module.exports = router;
