fd2a821f064845583478884cf6872d2ae6084e47
Spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2018-03-11 17:22:20 +0100

    should fix the app crashing when API is down

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var _ = require('underscore')._;
+var _ = require('underscore');
 var request = require('request');
 var router = express.Router();
 
@@ -7,24 +7,24 @@ var drugCache = {};
 var benzoCache = {};
 
 function getAlias() {
- _.each(benzoCache, function(benzo) {
-  _.each(benzo.aliases, function(alias) {
-   benzoCache.push({
-    // Add used properties to new objects
-    name: alias,
-    pretty_name: alias.charAt(0).toUpperCase() + alias.slice(1),
-    properties: benzo.properties,
-    formatted_dose: benzo.formatted_dose,
+  _.each(benzoCache, function (benzo) {
+    _.each(benzo.aliases, function (alias) {
+      benzoCache.push({
+        // Add used properties to new objects
+        name: alias,
+        pretty_name: alias.charAt(0).toUpperCase() + alias.slice(1),
+        properties: benzo.properties,
+        formatted_dose: benzo.formatted_dose,
+      });
     });
   });
- }); 
 }
 
 function sortCache() {
   benzoCache = _.sortBy(benzoCache, 'name');
   // Apply a value Select2 can use to calculate with
   var regex = /[0-9]+\.?[0-9]?/; // search for the first number sequence of a number in a sentence
-  benzoCache = _.each(benzoCache, function(benzoCache) {
+  benzoCache = _.each(benzoCache, function (benzoCache) {
     benzoCache.diazvalue = regex.exec(benzoCache.properties.dose_to_diazepam);
   });
 }
@@ -32,26 +32,30 @@ function sortCache() {
 function formatCache() {
   benzoCache = {};
   // Filter any drug not containing the dose_to_diazepam property
-  benzoCache = _.filter((drugCache), function(drugCache) {
-   return _.has(drugCache.properties, 'dose_to_diazepam');
- });
+  benzoCache = _.filter((drugCache), function (drugCache) {
+    return _.has(drugCache.properties, 'dose_to_diazepam');
+  });
 }
 
-var updateCache = function() {
- // Get the drugs
- request.get('http://tripbot.tripsit.me/api/tripsit/getAllDrugs', {
-  'json': true,
- }, 
- function(request, response, body) {
-  // Get cache
-  drugCache = body.data[0];
-  // Format Dose
-  formatCache();
-  // Get all aliases
-  getAlias();
-  // Sort cache
-  sortCache();
- });
+var updateCache = function () {
+  // Get the drugs
+  request.get('http://tripbot.tripsit.me/api/tripsit/getAllDrugs', {
+    json: true,
+  },
+  function (request, response, body) {   
+     if (response.statusCode === 201 || response.statusCode === 200) {
+      // Get cache
+      drugCache = body.data[0];
+      // Format Dose
+      formatCache();
+      // Get all aliases
+      getAlias();
+      // Sort cache
+      sortCache();
+  } else {
+      console.log("updating drug cache failed: " + response.statusCode);
+    }
+  });
 };
 
 // Update from our cache every minute, update from erowid's api every 60 minutes
@@ -59,8 +63,8 @@ setInterval(updateCache, 60000);
 updateCache();
 
 // Routes
-router.get("/", function(req, res) {
-  res.render("index", {benzo: benzoCache});
+router.get('/', function (req, res) {
+  res.render('index', { benzo: benzoCache });
 });
 
 module.exports = router;
